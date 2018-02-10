b16f30aeaee2489c0ccbaa15ba296aed4048719d
Victor Castaño <victorcastanogutierrez@gmail.com>
2018-02-10 16:55:46 +0100

    Model

---
+++ server/src/main/java/com/uniovi/web/services/model/BlogPost.java
@@ -0,0 +1,115 @@
+package com.uniovi.web.services.model;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "BLOGPOST", uniqueConstraints = @UniqueConstraint(columnNames = { "CREATOR_ID", "TITLE" }))
+public class BlogPost extends BaseEntity {
+
+	@ManyToOne
+	@JoinColumn(name = "CREATOR_ID")
+	private User creator;
+
+	@Column(name = "TITLE", nullable = false)
+	private String title;
+
+	@Lob
+	@Column(name = "BODY", nullable = false, length = 512)
+	private String body;
+
+	@Column(name = "CREATION_DATE", nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date creationDate;
+
+	@ManyToMany
+	@JoinTable(name = "BLOGPOST_TAG")
+	private Set<Tag> tags;
+
+	public BlogPost() {
+
+	}
+
+	public User getCreator() {
+		return creator;
+	}
+
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Set<Tag> getTags() {
+		return tags;
+	}
+
+	public void setTags(Set<Tag> tags) {
+		this.tags = tags;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((creator == null) ? 0 : creator.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BlogPost other = (BlogPost) obj;
+		if (creator == null) {
+			if (other.creator != null)
+				return false;
+		} else if (!creator.equals(other.creator))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+}
